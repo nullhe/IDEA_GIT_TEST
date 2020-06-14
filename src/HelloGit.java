21fff9218a6d2f20aa26dd0c61e2ff7fa44581cd
hepuyin <1548513100@qq.com>
2020-06-14 17:12:25 +0800

    测试提交 Idea_Git

---
+++ src/HelloGit.java
@@ -0,0 +1,10 @@
+/**
+ * Idea上Git测试
+ * @author null_he
+ * @create 2020-06-14 17:11
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
